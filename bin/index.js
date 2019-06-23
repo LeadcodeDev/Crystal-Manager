85c75a5a9aa9dfc01d046ef957b632f472e25318
freeze455 <freezeminecraftandgaming@gmail.com>
2019-06-23 23:20:02 +0200

    UPDATE SYSTEM

--- bin/index.js
+++ bin/index.js
@@ -4,51 +4,61 @@ const chalk = require('chalk')
 const fs = require('fs')
 const args = process.argv.slice(2)
 
-console.clear()
+shell.config.silent = true;
 
-if (args[0])
-{
-    let execType = args[0].split(':')
+if (!args[0]) return console.error(`${chalk.redBright(`ERROR : Invalid usage.\nPlease select an option "crystal make:project <name>"`)}`)
 
-    if (execType[0] === "make")
-    {
-        switch (execType[1])
-        {
-            case "project":
-                if (args[1])
-                {
-                    console.log(`${chalk.bgMagenta(`Project creation :`)} ${chalk.magenta(args[1])}`)
-                    shell.exec(`git clone https://github.com/Freeze455/Discord-Bot-Template.git ${args[1]} && cd ${args[1]}`)
-                    console.log(`${chalk.yellow("SUCCES : Project creation successfully")}`)
-                }
-                else
-                {
-                    console.log("ERROR : Please specify a name for the project")
-                }
-            break
 
-            case "command":
-                console.log("Creating the command file")
-                let contentFile = 
-                    'exports.run = (client, message, args) => {\n\n' +
-                    '    // Your code\n\n' +
-                    '}'
-                    
-                fs.writeFileSync(`commands/${args[1]}.js`, contentFile, "UTF-8")
-                console.log(`${chalk.yellow("SUCCES : Creating the command file successfully")}`)
-            break
+let execType = args[0].split(':')
 
-            default:
-                console.log(`${chalk.redBright('\nERROR : Please select a true option "ice make:[project | command] <name>"')}`)
-            break
+if (execType[0] == "make") {
+
+    if (!execType[1]) return console.error(`${chalk.redBright("ERROR : Invalid usage ")}`)
+    if (execType[1] == "project") {
+
+        if (!args[1]) return console.error(`${chalk.redBright("ERROR : Invalid usage ")}`)
+        console.log(`${chalk.green("\nProject creation :")} ${chalk.green(args[1])}\n`)
+        
+        if (shell.exec(`git clone https://github.com/Freeze455/Discord-Bot-Template.git ${args[1]} && cd ${args[1]}`).code !== 0) {
+            console.error(`${chalk.redBright("Error: Your project already exists")}`);
+            shell.exit(1);
+        } else {
+            console.log("Download template from https://github.com/Freeze455/Discord-Bot-Template.git\n")
+            fs.readdir(`./${args[1]}/events/`, (err, events) => {
+            
+                if (err) return console.error(err)
+                console.log(chalk.yellow("Loading events\n"))
+
+                for (let i = 0; i < events.length; i++) {
+
+                    setTimeout(() => {
+
+                        console.log(`- ${events[i].split(".")[0]} (100%)`)
+                        if (i == events.length -1) {
+
+                            console.log(chalk.yellow("\n\nLoading commands\n"))
+
+                            fs.readdir(`./${args[1]}/commands/`, (err, commands) => {
+                                if (err) return console.error(err)
+
+                                for (let i = 0; i < commands.length; i++) {
+                                    setTimeout(() => {
+                                        console.log(`- ${commands[i].split(".")[0]} (100%)`)
+                                        
+                                        if (i == commands.length -1) {
+                                            let targetFolder = __dirname.split('\\')
+                                            console.log(`\nYour download location is "${targetFolder.slice(0, targetFolder.length -2).join("/")}/${args[1]}"\n`)
+                                            console.log(`${chalk.green("\nSUCCES : Project creation successfully\n")}`)
+                                        }
+                                    }, i * 50)           
+                                }
+                            })
+                        }
+                    }, i * 50)
+                }                
+            })  
         }
+    } else {
+        return console.error(`${chalk.redBright("ERROR : Invalid usage 3")}`)
     }
-    else
-    {
-        console.log(`${chalk.redBright('ERROR : Please select "make" option "ice make:[project | command] <name>"')}`)
-    }
-}
-else
-{
-    console.log(`${chalk.redBright(`ERROR : Invalid usage.\nPlease select an option "ice make:[project | command] <name>"`)}`)
 }
